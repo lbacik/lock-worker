130d0b722c90a098a4c9efabd648b63d2e6d89d9
Łukasz Bacik <mail@luka.sh>
2023-03-24 17:41:20 +0100

    Fix mutex

--- worker/mutex/redis.py
+++ worker/mutex/redis.py
@@ -10,10 +10,12 @@ class RedisMutex(Mutex):
 
     def __init__(self, host: str = 'localhost', port: int = 6379, db: int = 0) -> None:
         self.redis: Redis[bytes] = Redis(host=host, port=port, db=db)
-        self.lock = Lock(self.redis, self.MUTEX_KEY_NAME, timeout=30, blocking_timeout=60)
+        self.lock = Lock(self.redis, self.MUTEX_KEY_NAME, timeout=None, blocking_timeout=5)
 
     def acquire(self) -> None:
-        self.lock.acquire(blocking=True)
+        while not self.lock.acquire(blocking=True):
+            print('Waiting for lock...')
+        print('Lock acquired')
 
     def release(self) -> None:
         self.lock.release()
